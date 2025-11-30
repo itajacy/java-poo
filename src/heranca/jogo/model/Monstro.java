a250727028843865bf04cef0159a0b7b848d5b54
ita Dev <itajacy@gmail.com>
2025-11-30 15:02:29 -0300

    aula 20 - Herança - exemplo 2 - Monstro criado, teste ok

---
+++ src/heranca/jogo/model/Monstro.java
@@ -0,0 +1,16 @@
+package heranca.jogo.model;
+
+
+public class Monstro extends Jogador{
+
+    public Monstro() {
+
+    }
+
+    public Monstro(int x, int y) {
+        super(x,y,1,5);
+    }
+
+
+
+}
--- src/heranca/jogo/teste/TesteJogo.java
+++ src/heranca/jogo/teste/TesteJogo.java
@@ -3,17 +3,18 @@ package heranca.jogo.teste;
 import heranca.jogo.model.Direcao;
 import heranca.jogo.model.Heroi;
 import heranca.jogo.model.Jogador;
+import heranca.jogo.model.Monstro;
 
 public class TesteJogo {
     static void main() {
 
         Jogador j1 = new Heroi(20,50);
-        Jogador j2 = new Jogador(23,50);
+        Jogador j2 = new Monstro(21,50);
 
 //        System.out.println(j1.mover(Direcao.NORTE));
 
         j1.atacar(j2);
-
+        j2.atacar(j1);
 
 
         System.out.println(j1);
